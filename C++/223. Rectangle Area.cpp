2a9055bb52778bf0cc2926ad8ee89659181e1183
unknown <zefengsong@gwmail.gwu.edu>
2017-08-07 17:10:46 +0800

    Create 223. Rectangle Area

---
+++ C++/223. Rectangle Area.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+	int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+		int areaA = (C - A) * (D - B);
+		int areaB = (G - E) * (H - F);
+		if (C < E || A > G || B > H || D < F) return areaA + areaB;
+		// (I, J), (K, L).
+		int I = max(A, E);
+		int J = max(B, F);
+		int K = min(C, G);
+		int L = min(D, H);
+		int overlap = (K - I) * (L - J);
+		return areaA + areaB - overlap;
+	}
+};
