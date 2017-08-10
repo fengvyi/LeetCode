00287a5f77c32b56c7e5033708573be747c2a37b
unknown <zefengsong@gwmail.gwu.edu>
2017-08-10 17:28:56 +0800

    Create 236. Lowest Common Ancestor of a Binary Tree.cpp

---
+++ C++/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -0,0 +1,38 @@
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+// Solution 1. Brute force, T(n) = T(n/2) + O(n); Time complexity : O(n)
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (root == p || root == q) return root;
+		bool foundP = found(root->left, p);
+		bool foundQ = found(root->right, q);
+		if (foundP && foundQ || !foundP && !foundQ) return root;
+		if (foundP) return lowestCommonAncestor(root->left, p, q);
+		return lowestCommonAncestor(root->right, p, q);
+	}
+
+	bool found(TreeNode* root, TreeNode* target) {
+		if (!root) return false;
+		if (root == target) return true;
+		return found(root->left, target) || found(root->right, target);
+	}
+};
+
+// Solution 2. O(logn)
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (!root || root == p || root == q) return root;
+		TreeNode* left = lowestCommonAncestor(root->left, p, q);
+		TreeNode* right = lowestCommonAncestor(root->right, p, q);
+		return !left ? right : !right ? left : root;
+	}
+};
