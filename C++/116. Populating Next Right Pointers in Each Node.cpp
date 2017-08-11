2ee4b3eeb725a4e7d2826c3449281df50ef8537f
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-11 17:29:09 +0800

    Update 116. Populating Next Right Pointers in Each Node.cpp

--- C++/116. Populating Next Right Pointers in Each Node.cpp
+++ C++/116. Populating Next Right Pointers in Each Node.cpp
@@ -23,3 +23,19 @@ public:
         }
     }
 };
+
+// Share a smart O(1) space solution from this thread: https://discuss.leetcode.com/topic/2202/a-simple-accepted-solution.
+void connect(TreeLinkNode *root) {
+    if (root == NULL) return;
+    TreeLinkNode *pre = root;
+    TreeLinkNode *cur = NULL;
+    while(pre->left) {
+        cur = pre;
+        while(cur) {
+            cur->left->next = cur->right;
+            if(cur->next) cur->right->next = cur->next->left;
+            cur = cur->next;
+        }
+        pre = pre->left;
+    }
+}
