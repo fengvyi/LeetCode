8951e2f16e4b71c8944cebfc59b458742e4c1c61
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-12 20:18:22 +0800

    Create 208. Implement Trie (Prefix Tree).cpp

---
+++ C++/208. Implement Trie (Prefix Tree).cpp
@@ -0,0 +1,24 @@
+class Trie {
+private:
+    unordered_set<string>s;
+public:
+    Trie() {}
+    
+    void insert(string word) {
+        s.insert(word);
+    }
+    
+    bool search(string word) {
+        return s.count(word);
+    }
+    
+    bool startsWith(string prefix) {
+        for(auto x: s){
+            int pos = 0;
+            int maxlen = max(x.size(), prefix.size());
+            while(pos < maxlen && x[pos] == prefix[pos]) pos++;
+            if(pos == prefix.size()) return true; 
+        }
+        return false;
+    }
+};
