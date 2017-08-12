3e5508c19dfb19c2463bdee3fa6fc0fda5dfc5df
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-12 20:44:15 +0800

    Update 208. Implement Trie (Prefix Tree).cpp

--- C++/208. Implement Trie (Prefix Tree).cpp
+++ C++/208. Implement Trie (Prefix Tree).cpp
@@ -1,4 +1,5 @@
-// Yes, I cheat, it's not a tree, maybe the shortest solution of all.
+// Solution 1
+// My cheat solution, it's not a tree, but maybe the shortest solution of all.
 class Trie {
 private:
     unordered_set<string>s;
@@ -21,5 +22,68 @@ public:
             if(pos == prefix.size()) return true; 
         }
         return false;
+    }
+};
+
+// Solution 2
+// Normal solution from this top-voted thread:
+// https://discuss.leetcode.com/topic/13463/maybe-the-code-is-not-too-much-by-using-next-26-c
+class TrieNode
+{
+public:
+    TrieNode *next[26];
+    bool is_word;
+    
+    // Initialize your data structure here.
+    TrieNode(bool b = false)
+    {
+        memset(next, 0, sizeof(next));
+        is_word = b;
+    }
+};
+
+class Trie
+{
+    TrieNode *root;
+public:
+    Trie()
+    {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    void insert(string s)
+    {
+        TrieNode *p = root;
+        for(int i = 0; i < s.size(); ++ i)
+        {
+            if(p -> next[s[i] - 'a'] == NULL)
+                p -> next[s[i] - 'a'] = new TrieNode();
+            p = p -> next[s[i] - 'a'];
+        }
+        p -> is_word = true;
+    }
+
+    // Returns if the word is in the trie.
+    bool search(string key)
+    {
+        TrieNode *p = find(key);
+        return p != NULL && p -> is_word;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    bool startsWith(string prefix)
+    {
+        return find(prefix) != NULL;
+    }
+
+private:
+    TrieNode* find(string key)
+    {
+        TrieNode *p = root;
+        for(int i = 0; i < key.size() && p != NULL; ++ i)
+            p = p -> next[key[i] - 'a'];
+        return p;
     }
 };
