71a9759d3f0ea9c7647c3a925c4febacd730df08
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-14 13:41:28 +0800

    Update 229. Majority Element II.cpp

--- C++/229. Majority Element II.cpp
+++ C++/229. Majority Element II.cpp
@@ -1,3 +1,43 @@
+// Solution 1
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+       vector<int>res;
+       int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+       for(auto n: nums){
+           if(candidate1 == n) 
+               count1++;
+           else if(candidate2 == n)
+               count2++;
+           else if(count1 == 0){
+               candidate1 = n;
+               count1 = 1;
+           }
+           else if(count2 == 0){
+               candidate2 = n;
+               count2 = 1;
+           }
+           else{
+               count1--;
+               count2--;
+           }
+       }
+       count1 = 0;
+       count2 = 0;
+       for(auto n: nums)
+           if(candidate1 == n)
+               count1++;
+           else if(candidate2 == n)
+               count2++;
+       if(count1 > nums.size()/3)
+           res.push_back(candidate1);
+       if(count2 > nums.size()/3)
+           res.push_back(candidate2);
+       return res;
+    }
+};
+
+// Solution 2
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
