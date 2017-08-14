e0ee08c323973ebbb94555a919791bb3c3256020
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-14 11:56:06 +0800

    Update 012. Integer to Roman.cpp

--- C++/012. Integer to Roman.cpp
+++ C++/012. Integer to Roman.cpp
@@ -16,8 +16,8 @@ public:
     string intToRoman(int num) {
         //'I' = 1, 'X' = 10, 'C' = 100, 'M' = 1000, 'V' = 5, 'L' = 50, 'D' = 500;
         // Subtractive Notation
-        // Number	 4	 9	 40  90  400  900
-        // Notation	 IV	 IX	 XL	 XC	 CD   CM
+        // Number	 4	 9	 40     90      400     900
+        // Notation	 IV	 IX	 XL	XC	CD      CM
         string res = "";
         while(num >= 1000){
             num -= 1000;
