ab060e015818bf4b18983ed755f6e6278d80901a
unknown <zefengsong@gwmail.gwu.edu>
2017-08-17 23:29:08 +0800

    Create 311. Sparse Matrix Multiplication.cpp

---
+++ C++/311. Sparse Matrix Multiplication.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+		vector<vector<int>>res(A.size(), vector<int>(B[0].size()));
+		for (int i = 0; i < A.size(); i++)
+			for (int j = 0; j < B[0].size(); j++)
+				res[i][j] = helper(A[i], B, j);
+		return res;
+	}
+
+	int helper(vector<int>& v, vector<vector<int>>& B, int col) {
+		int res = 0;
+		for (int i = 0; i < v.size(); i++)
+			if (v[i] != 0 && B[i][col] != 0)
+				res += v[i] * B[i][col];
+		return res;
+	}
+};
