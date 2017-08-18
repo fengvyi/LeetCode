cc051ceadaab3433dd037a9e55ad12ed8204d520
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-19 05:17:17 +0800

    Update 211. Add and Search Word - Data structure design.cpp

--- C++/211. Add and Search Word - Data structure design.cpp
+++ C++/211. Add and Search Word - Data structure design.cpp
@@ -1,3 +1,4 @@
+// Solution 1. Hash Table, 62ms, 100%.
 class WordDictionary {
 private:
     unordered_map<int, vector<string>>words;
@@ -9,17 +10,14 @@ private:
         }
         return true;
     }
+
 public:
-    /** Initialize your data structure here. */
     WordDictionary() {}
     
-    /** Adds a word into the data structure. */
     void addWord(string word) {
         words[word.size()].push_back(word);
     }
     
-    /** Returns if the word is in the data structure. 
-    A word could contain the dot character '.' to represent any one letter. */
     bool search(string word) {
         for(auto s: words[word.size()])
             if(isEqual(s, word)) return true;
@@ -27,9 +25,54 @@ public:
     }
 };
 
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary obj = new WordDictionary();
- * obj.addWord(word);
- * bool param_2 = obj.search(word);
- */
+// Solution 2. Trie, 88ms.
+struct TrieNode{
+    bool isKey;
+    TrieNode* next[26];
+    TrieNode():isKey(false){
+        memset(next, NULL, sizeof(next));
+    }
+};
+
+class WordDictionary {
+private:
+    TrieNode* root;
+    
+    bool helper(string word, TrieNode* node){
+        for(int i = 0; i < word.size(); i++){
+            char c = word[i];
+            if(c != '.'){
+                if(!node->next[c - 'a']) return false;
+                node = node->next[c - 'a'];
+            }
+            else{
+                bool found = false;
+                int j = 0;
+                for(; j < 26; j++){
+                    if(node->next[j]) found |= helper(word.substr(i + 1), node->next[j]);
+                    if(found) return true;
+                }
+                if(j == 26) return false;
+            }
+        }
+        return node->isKey;
+    }
+
+public:
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    void addWord(string word) {
+        TrieNode* node = root;
+        for(auto c: word){
+            if(!node->next[c - 'a']) node->next[c - 'a'] = new TrieNode();
+            node = node->next[c - 'a'];
+        }
+        node->isKey = true;
+    }
+    
+    bool search(string word) {
+        return helper(word, root);
+    }
+};
