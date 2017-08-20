bdbb7a3bf46bf60ec82c8462803602081fa559cd
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-20 14:57:42 -0400

    Create 377. Combination Sum IV.cpp

---
+++ C++/377. Combination Sum IV.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    unordered_map<int, int>m;
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        if(target < 0) return 0;
+        if(target == 0) return 1;
+        if(m.count(target) > 0) return m[target];
+        int sum = 0;
+        for(auto  x: nums)
+            if(target >= x)
+                sum += combinationSum4(nums, target - x);
+        m[target] = sum;
+        return sum;
+    }
+};
