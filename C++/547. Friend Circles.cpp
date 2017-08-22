5202c608aeec690bd9d4573290fe62fd2ed81473
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-22 16:34:29 -0400

    Create 547. Friend Circles.cpp

---
+++ C++/547. Friend Circles.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& M) {
+        vector<int>Tags(M.size());
+        int tag = 0;
+        for(int i = 0; i < M.size(); i++){
+            if(!Tags[i]) Tags[i] = ++tag;
+            for(int j = 0; j < M[0].size(); j++){
+                if(j == i || M[i][j] == 0) continue;
+                if(M[i][j]){
+                    M[i][j] = M[j][i] = 0;
+                    DFS(Tags, M, j, tag);
+                }
+            }
+        }
+        return tag;
+    }
+    
+    void DFS(vector<int>& Tags, vector<vector<int>>& M, int row, int tag){
+        Tags[row] = tag;
+        for(int i = 0; i < M[row].size(); i++){
+            if(M[row][i]){
+                M[row][i] = M[i][row] = 0;
+                DFS(Tags, M, i, tag);
+            }
+        }
+    }
+};
