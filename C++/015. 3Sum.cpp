d495b44029d40d626ca64dd7320675b3b9699789
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-24 13:16:16 -0400

    Update 015. 3Sum.cpp

--- C++/015. 3Sum.cpp
+++ C++/015. 3Sum.cpp
@@ -3,15 +3,13 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>>res;
         sort(nums.begin(), nums.end());
-        int hi = nums.size() - 1;
-        for(int i = 0; i < hi - 1; i++){
+        for(int i = 0; i < (int)nums.size() - 1; i++){
             if(i > 0 && nums[i - 1] == nums[i]) continue;
-            int j = i + 1;
-            while(nums[i] + nums[j] + nums[hi] > 0) hi--;
-            for(int k = hi; k > j; k--){
-                if(k < nums.size() - 1 && nums[k] == nums[k + 1]) continue;
-                while(nums[j] < -(nums[i] + nums[k])) j++;
-                if(j < k && (nums[i] + nums[j] + nums[k]) == 0) res.push_back({nums[i], nums[j], nums[k]});
+            int lo = i + 1;
+            for(int hi = nums.size() - 1; hi > lo; hi--){
+                if(hi < nums.size() - 1 && nums[hi] == nums[hi + 1]) continue;
+                while(nums[lo] < -(nums[i] + nums[hi])) lo++;
+                if(lo < hi && (nums[i] + nums[lo] + nums[hi]) == 0) res.push_back({nums[i], nums[lo], nums[hi]});
             }
         }
         return res;
