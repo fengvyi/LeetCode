c79e54600e67d16e84a6e46a29aca56b6857417e
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-24 13:31:10 -0400

    Create 017. Letter Combinations of a Phone Number.cpp

---
+++ C++/017. Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        if(digits.size() == 0) return res;
+        string s = "";
+        unordered_map<char,vector<int>>m({{'2', {'a','b','c'}},
+                                          {'3', {'d','e','f'}},
+                                          {'4', {'g','h','i'}},
+                                          {'5', {'j','k','l'}},
+                                          {'6', {'m','n','o'}},
+                                          {'7', {'p','q','r','s'}},
+                                          {'8', {'t','u','v'}},
+                                          {'9', {'w','x','y','z'}}});
+        DFS(m, digits, 0, res, s);
+        return res;
+    }
+    
+    void DFS(unordered_map<char,vector<int>>& m, string& digits, int l, vector<string>& res, string& s){
+        if(l == digits.size()){
+            res.push_back(s);
+            return;
+        }
+        for(int i = 0; i < m[digits[l]].size(); i++){
+            s.push_back(m[digits[l]][i]);
+            DFS(m, digits, l+1, res, s);
+            s.pop_back();
+        }
+        return;
+    } 
+};
