0c5e0b64aafc2373216e1b75cee89a03af1eb8bb
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-24 15:37:32 -0400

    Create 033. Search in Rotated Sorted Array.cpp

---
+++ C++/033. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        if(n == 0) return -1;
+        int lo = 0, hi = n - 1;
+        int mid = (lo + hi) / 2;
+        while(lo < hi){
+            if(nums[mid] > nums[hi]) lo = mid + 1;
+            else hi = mid;
+            mid = (lo + hi) / 2;
+        }
+        int pos = (target >= nums[0] && lo != 0) ? lower_bound(nums.begin(), nums.begin() + lo, target) - nums.begin() 
+                                                 : lower_bound(nums.begin() + lo, nums.end(), target) - nums.begin();
+        return nums[pos] == target ? pos : -1;
+    }
+};
