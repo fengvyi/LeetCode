a65212c5fd728a746bf6d11cb3af46eaa380f12a
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-25 15:02:09 -0400

    Create 088. Merge Sorted Array.cpp

---
+++ C++/088. Merge Sorted Array.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = m - 1, j = n - 1; j >= 0;) nums1[i + j + 1] = (i < 0 || nums1[i] < nums2[j]) ? nums2[j--] : nums1[i--];
+    }
+};
