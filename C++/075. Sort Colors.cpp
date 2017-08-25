3d01b82fb284b1309f7aa6ac7d6bc9d8594bdb87
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-25 11:49:35 -0400

    Create 075. Sort Colors.cpp

---
+++ C++/075. Sort Colors.cpp
@@ -0,0 +1,17 @@
+/**
+ * 1. Put '0's to the low position.
+ * 2. Put '2's to the high position.
+ * 3. Jump '1's as it is.
+ */
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, high = nums.size()-1;
+        for(int i = 0; i <= high;)
+            if(nums[i] == 0) 
+                swap(nums[i++], nums[low++]);
+            else if(nums[i] == 2) 
+                swap(nums[i], nums[high--]);
+            else i++;
+    }
+};
