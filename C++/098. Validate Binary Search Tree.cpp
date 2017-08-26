622e792bcf5086e905c5bddb1615bee77669228d
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-25 21:45:37 -0400

    Update 098. Validate Binary Search Tree.cpp

--- C++/098. Validate Binary Search Tree.cpp
+++ C++/098. Validate Binary Search Tree.cpp
@@ -7,15 +7,30 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// Solution 1.
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
         if(!root) return true;
-        if(!helper(root->left, root->val, true) || !helper(root->right, root->val, false)) return false;
+        if(!isValid(root->left, root->val, true) || !isValid(root->right, root->val, false)) return false;
         return isValidBST(root->left) && isValidBST(root->right);
     }
     
-    bool helper(TreeNode* root, int bound, bool isLeft){
-        return !root || (isLeft ? root->val < bound : root->val > bound ) && helper(root->left, bound, isLeft) && helper(root->right, bound, isLeft);
+    bool isValid(TreeNode* root, int bound, bool isLeft){
+        return !root || (isLeft ? root->val < bound : root->val > bound ) && isValid(root->left, bound, isLeft) && isValid(root->right, bound, isLeft);
+    }
+};
+
+// Solution 2. 
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValid(root, NULL, NULL);
+    }
+    
+    bool isValid(TreeNode* root, TreeNode* minNode, TreeNode* maxNode){
+        if(!root) return true;
+        if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val) return false;
+        return isValid(root->left, minNode, root) && isValid(root->right, root, maxNode);
     }
 };
