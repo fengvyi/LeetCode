bb9810ac33ca83a7345959ce14a2a3e3282124e1
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-26 16:13:07 -0400

    Create 127. Word Ladder.cpp

---
+++ C++/127. Word Ladder.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        deque<string>cur;
+        deque<string>next;
+        cur.push_back(beginWord);
+        int len = 2;
+        while(!cur.empty()){
+            string node = cur.front();
+            cur.pop_front();
+            for(auto& x: wordList){
+                if(x == "" || !isNeighbor(node, x)) continue;
+                if(x == endWord) return len;
+                next.push_back(x);
+                x = "";
+            }
+            if(cur.empty()){
+                len++;
+                swap(cur, next);
+            }
+        }
+        return 0;
+    }
+    
+    bool isNeighbor(string& a, string& b){
+        int diff = 0;
+        for(int i = 0; i < a.size(); i++) if(a[i] != b[i]) diff++;
+        return diff == 1;
+    }
+};
