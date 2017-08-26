c2ce2e24c697b9d2106cfe1ce5f1e85d430eab37
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-25 22:03:01 -0400

    Create 102. Binary Tree Level Order Traversal.cpp

---
+++ C++/102. Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,44 @@
+// SOlution 1. BFS
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>res;
+        if(!root) return res;
+        deque<TreeNode*>cur;
+        deque<TreeNode*>next;
+        cur.push_back(root);
+        int level = 0;
+        res.push_back(vector<int>());
+        while(!cur.empty()){
+            TreeNode* node = cur.front();
+            cur.pop_front();
+            if(node->left) next.push_back(node->left);
+            if(node->right) next.push_back(node->right);
+            res[level].push_back(node->val);
+            if(cur.empty() && !next.empty()){
+                res.push_back(vector<int>());
+                level++;
+                swap(cur, next);
+            }
+        }
+        return res;
+    }
+};
+
+// Solution 2. DFS
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        DFS(root, 1, res);
+        return res;
+    }
+    
+    void DFS(TreeNode* root, int level, vector<vector<int>>& res){
+        if(!root) return;
+        if(level > res.size()) res.push_back(vector<int>(1, root->val));
+        else res[level - 1].push_back(root->val);
+        DFS(root->left, level + 1, res);
+        DFS(root->right, level + 1, res);
+    }
+};
