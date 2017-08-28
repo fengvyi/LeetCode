6cacda754d87827abff1695c3dc63fbc496bdf53
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-28 12:13:34 -0400

    Create 535. Encode and Decode TinyURL.cpp

---
+++ C++/535. Encode and Decode TinyURL.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        v.push_back(longUrl);
+        return to_string(v.size() - 1);
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        int i = stoi(shortUrl);
+        return v[i];
+    }
+    
+private:
+    vector<string>v;
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
+
+// Best answer (joke).
+class Solution {
+public:
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return shortUrl;
+    }
+};
