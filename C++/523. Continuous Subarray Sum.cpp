5408812653f53bf110d201d0daf1ff19ea091652
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-28 16:14:25 -0400

    Create 523. Continuous Subarray Sum.cpp

---
+++ C++/523. Continuous Subarray Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    unordered_map<int, int>m;
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        k = abs(k);
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            if(i < nums.size() - 1 && nums[i] == 0 && nums[i + 1] == 0) return true;
+            if(k != 0 && sum % k == 0 && i > 0) return true;
+            for(int j = 1; k != 0 && j*k < sum; j++)
+                if(m.count(sum - j*k) > 0 && i - m[sum - j*k] > 0) return true;
+            m[sum] = i;
+        }
+        return false;
+    }
+};
