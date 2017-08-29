f50f4dee3ef454966b613a13098364b7d27d5355
fengvyi <zefengsong@gwmail.gwu.edu>
2017-08-29 18:30:11 -0400

    Create 113. Path Sum II.cpp

---
+++ C++/113. Path Sum II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>>res;
+        if(!root) return res;
+        DFS(root, res, vector<int>(), 0, sum);
+        return res;
+    }
+    
+    void DFS(TreeNode* root, vector<vector<int>>& res, vector<int>path, int sum, int target){
+        if(!root) return;
+        path.push_back(root->val);
+        sum += root->val;
+        if(!root->left && !root->right){
+            if(sum == target) res.push_back(path);
+            return;
+        }
+        DFS(root->left, res, path, sum, target);
+        DFS(root->right, res, path, sum, target);
+    }
+};
