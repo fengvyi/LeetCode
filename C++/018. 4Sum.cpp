41d3f1466cc7d2fad368ac566dcd81a380c679a1
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-01 00:46:04 -0400

    Update 018. 4Sum.cpp

--- C++/018. 4Sum.cpp
+++ C++/018. 4Sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
         vector<vector<int>>res;
         vector<int>path;
         DFS(res, nums, 0, target, 0, 0, path);
@@ -13,8 +14,11 @@ public:
             return;
         }
         for(int i = pos; i < nums.size(); i++){
+            if(i != pos && nums[i] == nums[i - 1]) continue;
+            if(sum + nums[i] + (3 - count) * nums[nums.size() - 1] < target) continue;
+            if(sum + (4 - count)* nums[i] > target) break;
             path.push_back(nums[i]);
-            DFS(res, nums, pos + 1, target, count + 1, sum + nums[i], path);
+            DFS(res, nums, i + 1, target, count + 1, sum + nums[i], path);
             path.pop_back();
         }
     }
