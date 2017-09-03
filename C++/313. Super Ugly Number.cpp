ee93aade10d5777888764d9d2bd0d018a6c36fbb
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-03 19:38:07 -0400

    Update 313. Super Ugly Number.cpp

--- C++/313. Super Ugly Number.cpp
+++ C++/313. Super Ugly Number.cpp
@@ -1,3 +1,4 @@
+// Solution 1. O(n*2n)
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
@@ -13,13 +14,16 @@ public:
     }
 };
 
-// Trade-off space for time, beats 97.16%.
+// Solution 2.
+// Improve from 2 inner loops to one loop, idea from this post: 
+// https://discuss.leetcode.com/topic/34841/java-three-methods-23ms-36-ms-58ms-with-heap-performance-explained
+```
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
         vector<int>dp(n);
         vector<int>p(primes.size());
-        vector<int>val(primes.size(), 1);
+        vector<int>val(primes.size());
         int next = 1;
         for(int i = 0; i < n; i++){
             dp[i] = next;
@@ -27,6 +31,37 @@ public:
             for(int j = 0; j < val.size(); j++){
                 if(dp[i] >= val[j]) val[j] = dp[p[j]++] * primes[j];
                 next = min(next, val[j]);
+            }
+        }
+        return dp[n - 1];
+    }
+};
+
+// Solution 3.
+// Also, I wrote the last heap version from the same post, but I think its time complexity is O(nklogk) rather than O(nlogk), 
+// which is actually slower than above solution.
+struct triple{
+    int val;
+    int prime;
+    int p;
+    triple(int x, int y, int z):val(x), prime(y), p(z){};
+};
+
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        vector<int>dp(n);
+        auto cmp = [](triple* t1, triple* t2){ return t1->val > t2->val; };
+        priority_queue<triple*, vector<triple*>, decltype(cmp)>pq(cmp);
+        for(int i = 0; i < primes.size(); i++) 
+            pq.push(new triple(primes[i], primes[i], 1));
+        dp[0] = 1;
+        for(int i = 1; i < n; i++){
+            dp[i] = pq.top()->val;
+            while(pq.top()->val == dp[i]){
+                triple* t = pq.top();
+                pq.pop();
+                pq.push(new triple(t->prime * dp[t->p], t->prime, t->p + 1));
             }
         }
         return dp[n - 1];
