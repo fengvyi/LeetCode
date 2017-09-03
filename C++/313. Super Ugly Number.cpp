14f4056c2e97df7368437a6f75c37c34228f9cce
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-03 17:21:10 -0400

    Update 313. Super Ugly Number.cpp

--- C++/313. Super Ugly Number.cpp
+++ C++/313. Super Ugly Number.cpp
@@ -12,3 +12,23 @@ public:
         return dp[n - 1];
     }
 };
+
+// Trade-off space for time, beats 96.63%.
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        vector<int>dp(n);
+        vector<int>p(primes.size());
+        vector<int>val(primes.size(), 1);
+        int next = 1;
+        for(int i = 0; i < n; i++){
+            dp[i] = next;
+            next = INT_MAX;
+            for(int j = 0; j < val.size(); j++){
+                if(dp[i] >= val[j]) val[j] = dp[p[j]++] * primes[j];
+                next = min(next, val[j]);
+            }
+        }
+        return dp[n - 1];
+    }
+};
