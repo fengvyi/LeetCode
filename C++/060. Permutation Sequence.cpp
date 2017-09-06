97aa402928c0278673287f08c1abf565904fcb3e
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-06 00:20:10 -0400

    Update 060. Permutation Sequence.cpp

--- C++/060. Permutation Sequence.cpp
+++ C++/060. Permutation Sequence.cpp
@@ -1,3 +1,4 @@
+// Solution 1. Backtracking
 class Solution {
 public:
     string getPermutation(int n, int k) {
@@ -21,5 +22,16 @@ public:
             DFS(s, pos + 1, count, n, k, path, res);
             s[i] = path[pos];
         }
+    }
+};
+
+// Solution 2. Using STL
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s = "";
+        for(int i = 1; i <= n; i++) s.push_back(i + '0');
+        while(--k) next_permutation(s.begin(), s.end());
+        return s;
     }
 };
