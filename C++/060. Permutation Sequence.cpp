5c36851dc7db6954610899b4eb1c1ba91965433e
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-06 00:15:45 -0400

    Update 060. Permutation Sequence.cpp

--- C++/060. Permutation Sequence.cpp
+++ C++/060. Permutation Sequence.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        string s = "";
+        string s = "", res = "";
         for(int i = 1; i <= n; i++) s.push_back(i + '0');
-        string res = "";
         string path = s;
         int count = 0;
         DFS(s, 0, count, n, k, path, res);
@@ -11,10 +10,8 @@ public:
     }
     
     void DFS(string& s, int pos, int& count, int n, int k, string& path, string& res){
-        if(count == k) return;
-        if(pos == n){
-            count++;
-            if(count == k) res = path;
+        if(count >= k || pos == n){
+            if(++count == k) res = path;
             return;
         }
         for(int i = 0; i < n; i++){
@@ -24,6 +21,5 @@ public:
             DFS(s, pos + 1, count, n, k, path, res);
             s[i] = path[pos];
         }
-        
     }
 };
