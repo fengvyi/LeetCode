d1acfecd5afe724e19a3142ad6aebd4c3169cba2
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-07 21:14:10 -0400

    Update 547. Friend Circles.cpp

--- C++/547. Friend Circles.cpp
+++ C++/547. Friend Circles.cpp
@@ -4,7 +4,8 @@ public:
         vector<int>Tags(M.size());
         int tag = 0;
         for(int i = 0; i < M.size(); i++){
-            if(!Tags[i]) Tags[i] = ++tag;
+            if(Tags[i]) continue;
+            Tags[i] = ++tag;
             for(int j = 0; j < M[0].size(); j++){
                 if(j == i || M[i][j] == 0) continue;
                 if(M[i][j]){
