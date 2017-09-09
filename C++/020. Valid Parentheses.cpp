ee9db634c0db85a6f8b376fd4442792953d23f99
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-09 17:44:53 -0400

    Update 020. Valid Parentheses.cpp

--- C++/020. Valid Parentheses.cpp
+++ C++/020. Valid Parentheses.cpp
@@ -3,11 +3,13 @@ public:
     bool isValid(string s) {
         stack<char>stk;
         for(auto c: s){
-            if(stk.empty() && c != '(' && c != '[' && c != '{') return false;
-            else if((c == ')' && stk.top() != '(') || (c == ']' && stk.top() != '[') || (c == '}' && stk.top() != '{'))
-                return false;
-            if(c == '(' || c == '[' || c== '{') stk.push(c);
-            else stk.pop();
+            if(stk.empty() && (c == ')' || c == ']' || c == '}')) return false;
+            if(c == '(' || c == '[' || c == '{') stk.push(c);
+            else{
+                char left = stk.top();
+                if((c == ')' && left != '(') || (c == ']' && left != '[') || (c == '}' && left != '{')) return false;
+                stk.pop();
+            }
         }
         return stk.empty();
     }
