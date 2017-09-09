76b4c12d07bfd9f9a608676b27227cb935f6ba70
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-09 16:38:45 -0400

    Update 010. Regular Expression Matching.cpp

--- C++/010. Regular Expression Matching.cpp
+++ C++/010. Regular Expression Matching.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         if(p.empty()) return s.empty();
-        if(s.empty()){
-            if(p.empty()) return true;
-            if(p[1] != '*') return false;
-            return isMatch(s, p.substr(2));
-        }
+        if(s.empty()) return p.empty() || (p[1] == '*' ? isMatch(s, p.substr(2)) : false);
         if(p[0] != '.' && s[0] != p[0]) return p[1] == '*' ? isMatch(s, p.substr(2)) : false;
         if(p[1] == '*') return isMatch(s.substr(1), p) || isMatch(s, p.substr(2));
         return isMatch(s.substr(1), p.substr(1));
