7f137987d2111dbd570b8438edbbb40562ef5a38
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-10 20:51:42 -0400

    Update 056. Merge Intervals.cpp

--- C++/056. Merge Intervals.cpp
+++ C++/056. Merge Intervals.cpp
@@ -1,13 +1,20 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
 class Solution {
 public:
     vector<Interval> merge(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](Interval& a, Interval& b){ return a.start < b.start; });
         vector<Interval>res;
-        if(intervals.size() == 0) return res;
-        sort(intervals.begin(), intervals.end(), [](Interval& a, Interval& b){ return a.start < b.start;});
-        res.push_back(intervals[0]);
-        for(int i = 1; i < intervals.size(); i++)
-            if(intervals[i].start <= res.back().end) res.back().end = max(res.back().end, intervals[i].end);
-            else res.push_back(intervals[i]);
+        for(int i = 0; i < intervals.size(); i++)
+            if(res.empty() || res.back().end < intervals[i].start) res.push_back(intervals[i]);
+            else res.back().end = max(res.back().end, intervals[i].end);
         return res;
     }
 };
