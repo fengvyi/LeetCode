371f457a8e5001a926c632a54da99499fc4bd0bb
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-11 14:19:34 -0400

    Update 067. Add Binary.cpp

--- C++/067. Add Binary.cpp
+++ C++/067. Add Binary.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int carry = 0;
-        string sum = "";
-        int i = a.size() - 1;
-        int j = b.size() - 1;
+        int i = a.size() - 1, j = b.size() - 1, carry = 0;
+        string s = "";
         while(i >= 0 || j >= 0 || carry){
-            int num1 = i < 0 ? 0 : a[i] - '0';
-            int num2 = j < 0 ? 0 : b[j] - '0';
-            int k = num1 + num2 + carry;
-            sum.push_back(k % 2 + '0');
-            carry = k > 1 ? 1 : 0;
-            i--;
-            j--;
+            int sum = (i < 0 ? 0 : a[i--] - '0') + (j < 0 ? 0 : b[j--] - '0') + carry;
+            s = to_string(sum % 2) + s;
+            carry = sum / 2;
         }
-        reverse(sum.begin(), sum.end());
-        return sum;
+        return s;
     }
 };
