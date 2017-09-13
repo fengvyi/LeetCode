cc72898e8abfb12fda7d26bad7ff98d86312be31
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-13 15:52:03 -0400

    Update 116. Populating Next Right Pointers in Each Node.cpp

--- C++/116. Populating Next Right Pointers in Each Node.cpp
+++ C++/116. Populating Next Right Pointers in Each Node.cpp
@@ -6,36 +6,41 @@
  *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
  * };
  */
+
+// Solution 1. Works for any knid of tree, O(n) space.
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
         if(!root) return;
         deque<TreeLinkNode*>cur;
-        deque<TreeLinkNode*>sub;
+        deque<TreeLinkNode*>next;
         cur.push_back(root);
         while(!cur.empty()){
             TreeLinkNode* node = cur.front();
             cur.pop_front();
             node->next = cur.empty() ? NULL : cur.front();
-            if(node->left) sub.push_back(node->left);
-            if(node->right) sub.push_back(node->right);
-            if(cur.empty()) swap(cur, sub);
+            if(node->left) next.push_back(node->left);
+            if(node->right) next.push_back(node->right);
+            if(cur.empty()) swap(cur, next);
         }
     }
 };
 
-// Share a smart O(1) space solution from this thread: https://discuss.leetcode.com/topic/2202/a-simple-accepted-solution.
-void connect(TreeLinkNode *root) {
-    if (root == NULL) return;
-    TreeLinkNode *pre = root;
-    TreeLinkNode *cur = NULL;
-    while(pre->left) {
-        cur = pre;
-        while(cur) {
-            cur->left->next = cur->right;
-            if(cur->next) cur->right->next = cur->next->left;
-            cur = cur->next;
+// Solution 2. O(1) space.
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        TreeLinkNode* cur(root), *next;
+        while(cur){
+            next = cur->left;
+            while(cur){
+                if(cur->left){
+                    cur->left->next = cur->right;
+                    cur->right->next = cur->next ? cur->next->left : NULL;
+                }
+                cur = cur->next;
+            }
+            cur = next;
         }
-        pre = pre->left;
     }
-}
+};
