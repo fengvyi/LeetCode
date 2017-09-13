17ed2c2ab99de552301581bcf9bd8e19a490c7b9
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-13 14:50:17 -0400

    Update 098. Validate Binary Search Tree.cpp

--- C++/098. Validate Binary Search Tree.cpp
+++ C++/098. Validate Binary Search Tree.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-// Solution 1.
+// Solution 1. BF, O(n^2)
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
@@ -21,7 +21,7 @@ public:
     }
 };
 
-// Solution 2. 
+// Solution 2. D & C, O(n)
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
@@ -32,5 +32,26 @@ public:
         if(!root) return true;
         if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val) return false;
         return isValid(root->left, minNode, root) && isValid(root->right, root, maxNode);
+    }
+};
+
+// Solution 3. Iterative. O(n)
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        stack<TreeNode*>s;
+        TreeNode* pre = NULL;
+        while(root || !s.empty()){
+            while(root){
+                s.push(root);
+                root = root->left;
+            }
+            root = s.top();
+            s.pop();
+            if(pre && root->val <= pre->val) return false;
+            pre = root;
+            root = root->right;
+        }
+        return true;
     }
 };
