e9caa03164553e9765f1cf132a9a6e5ca36b2f18
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-14 19:39:33 -0400

    Update 158. Read N Characters Given Read4 II - Call multiple times.cpp

--- C++/158. Read N Characters Given Read4 II - Call multiple times.cpp
+++ C++/158. Read N Characters Given Read4 II - Call multiple times.cpp
@@ -1,6 +1,7 @@
 // Forward declaration of the read4 API.
 int read4(char *buf);
 
+// Solution 1.
 class Solution {
 private:
     int ptr = 0;
@@ -23,5 +24,32 @@ public:
             if(ptr == k) ptr = 0;
         }
         return cnt;
+    }
+};
+
+// Solution 2.
+class Solution {
+private:
+    int p = 4;
+    int i = 4;
+    char tmp[4];
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    int read(char *buf, int n) {
+        int sum = 0;
+        while(sum < n){
+            if(p == i){
+                i = read4(tmp);
+                p = 0;
+            }
+            if(i == 0) break;
+            *buf++ = tmp[p++];
+            sum++;
+        }
+        return sum;
     }
 };
