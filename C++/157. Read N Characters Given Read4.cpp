012062881e20cda88d6e07b951a1f2a599f8b140
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-14 19:18:16 -0400

    Update 157. Read N Characters Given Read4.cpp

--- C++/157. Read N Characters Given Read4.cpp
+++ C++/157. Read N Characters Given Read4.cpp
@@ -1,6 +1,7 @@
 // Forward declaration of the read4 API.
 int read4(char *buf);
 
+// Solution 1.
 class Solution {
 public:
     /**
@@ -13,6 +14,25 @@ public:
         for(int i = 0; i < n; i += 4){
             sum += read4(buf);
             buf += 4;
+        }
+        return min(sum, n);
+    }
+};
+
+// Solution 2.
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    int read(char *buf, int n) {
+        int sum = 0, i = 4;
+        while(sum < n && i == 4){
+            i = read4(buf);
+            buf += 4;
+            sum += i;
         }
         return min(sum, n);
     }
