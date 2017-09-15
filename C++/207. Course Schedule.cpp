40b08a84ea312e86b51af1c5ca25e88fbc8cf6a2
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-14 21:42:41 -0400

    Update 207. Course Schedule.cpp

--- C++/207. Course Schedule.cpp
+++ C++/207. Course Schedule.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
-        vector<vector<int>>graph(numCourses);
-        for(auto p: prerequisites) graph[p.second].push_back(p.first);
         vector<int>indegree(numCourses);
-        for(auto x: graph) for(auto y: x) indegree[y]++;
+        vector<vector<int>>graph(numCourses);
+        for(auto p: prerequisites){
+            graph[p.second].push_back(p.first);
+            indegree[p.first]++;
+        }
         for(int i = 0; i < numCourses; i++){
             int j = 0;
             for(; j < numCourses; j++) if(indegree[j] == 0) break;
             if(j == numCourses) return false;
-            for(auto x: graph[j]) indegree[x]--;
             indegree[j] = -1;
+            for(auto x: graph[j]) indegree[x]--;
         }
         return true;
     }
