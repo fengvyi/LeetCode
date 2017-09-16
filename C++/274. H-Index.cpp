fe9f931c6bec27caf66cac12c6a18dc68e4e7757
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-15 20:00:59 -0400

    Update 274. H-Index.cpp

--- C++/274. H-Index.cpp
+++ C++/274. H-Index.cpp
@@ -1,3 +1,4 @@
+// Solution 1.
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
@@ -5,5 +6,21 @@ public:
         int i = 0, j = citations.size() - 1;
         while(j >= 0 && citations[j] > i) i++, j--;
         return i;
+    }
+};
+
+// Solution 2.
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        vector<int>buckets(n + 1);
+        for(auto x: citations) x >= n ? buckets[n]++ : buckets[x]++;
+        int count = 0;
+        for(int i = n; i >= 0; i--){
+            count += buckets[i];
+            if(count >= i) return i;
+        }
+        return 0;
     }
 };
