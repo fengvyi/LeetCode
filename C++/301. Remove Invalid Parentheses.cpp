13ed9e2fee8c3c37a54959d985f2e491590835b0
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-16 17:49:44 -0400

    Update 301. Remove Invalid Parentheses.cpp

--- C++/301. Remove Invalid Parentheses.cpp
+++ C++/301. Remove Invalid Parentheses.cpp
@@ -1,3 +1,4 @@
+// Solution 1. Brute force, 196ms.
 class Solution {
 public:
     vector<string> removeInvalidParentheses(string s) {
@@ -8,7 +9,7 @@ public:
     }
     
     void backtrack(vector<string>& res, string s, int pos, int move, int& minMove){
-        if(pos > s.size()) return;
+        if(pos > s.size() || move > minMove) return;
         if(isValid(s)){
             if(move < minMove) res.clear(), res.push_back(s), minMove = move;
             else if(move == minMove && find(res.begin(), res.end(), s) == res.end()) res.push_back(s);
@@ -20,7 +21,47 @@ public:
         backtrack(res, s, pos + 1, move, minMove);
     }
     
-    bool isValid(string s){
+    bool isValid(string& s){
+        int sum = 0;
+        for(auto c: s){
+            if(c == '(') sum++;
+            else if(c == ')') sum--;
+            if(sum < 0) return false;
+        }
+        return sum == 0;
+    }
+};
+
+// Solution 2. BFS, 63ms.
+class Solution {
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        unordered_set<string>visited;
+        vector<string>res;
+        deque<string>cur;
+        deque<string>next;
+        cur.push_back(s);
+        while(!cur.empty()){
+            s = cur.front();
+            cur.pop_front();
+            if(isValid(s)){
+                res.push_back(s);
+                continue;
+            }
+            for(int i = 0; i < s.size(); i++){
+                if(s[i] != '(' && s[i] != ')') continue;
+                string tmp = s.substr(0, i) + s.substr(i + 1);
+                if(visited.count(tmp) == 0){
+                    next.push_back(tmp);
+                    visited.insert(tmp);
+                }
+            }
+            if(cur.empty() && res.size() == 0) swap(cur, next);
+        }
+        return res;
+    }
+    
+    bool isValid(string& s){
         int sum = 0;
         for(auto c: s){
             if(c == '(') sum++;
