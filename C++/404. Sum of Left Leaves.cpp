d3798ad330ede9e057bdbd1b417280f801f07644
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-16 20:44:27 -0400

    Update 404. Sum of Left Leaves.cpp

--- C++/404. Sum of Left Leaves.cpp
+++ C++/404. Sum of Left Leaves.cpp
@@ -7,10 +7,32 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// Recursive.
 class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
         return !root ? 0 : (root->left && !root->left->left && !root->left->right ? root->left->val : 0) + 
                            sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    }
+};
+
+// Iterative
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(!root) return 0;
+        stack<TreeNode*>s;
+        int sum = 0;
+        while(!s.empty() || root){
+            while(root){
+                s.push(root);
+                root = root->left;
+                if(root && !root->left && !root->right) sum += root->val;
+            }
+            root = s.top()->right;
+            s.pop();
+        }
+        return sum;
     }
 };
