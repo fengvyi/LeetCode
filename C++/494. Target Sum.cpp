998eb64e8dcc06fc4407e579c7b35b7fca1e7039
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-16 22:50:14 -0400

    Update 494. Target Sum.cpp

--- C++/494. Target Sum.cpp
+++ C++/494. Target Sum.cpp
@@ -1,3 +1,4 @@
+// Backtracking
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
@@ -13,5 +14,29 @@ public:
         }
         backtrack(nums, S, sum + nums[pos], pos + 1, ways);
         backtrack(nums, S, sum - nums[pos], pos + 1, ways);
+    }
+};
+
+// DP
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum = 0;
+        for(auto x: nums) sum += x;
+        if(sum < S || -sum > S) return 0;
+        vector<int>cur(2 * sum + 1);
+        vector<int>next(2 * sum + 1);
+        cur[sum] = 1;
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < 2 * sum + 1; j++){
+                if(cur[j] != 0){
+                    next[j + nums[i]] += cur[j];
+                    next[j - nums[i]] += cur[j];
+                    cur[j] = 0;
+                }
+            }
+            swap(cur, next);
+        }
+        return cur[sum + S];
     }
 };
