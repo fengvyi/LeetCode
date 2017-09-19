c13cb8370829c5605b54e6c1589533ff79a36326
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-18 23:32:45 -0400

    Update 572. Subtree of Another Tree.cpp

--- C++/572. Subtree of Another Tree.cpp
+++ C++/572. Subtree of Another Tree.cpp
@@ -11,14 +11,11 @@ class Solution {
 public:
     bool isSubtree(TreeNode* s, TreeNode* t) {
         if(!s) return !t;
-        if(s->val == t->val && isEqual(s, t)) return true;
-        return isSubtree(s->left, t) || isSubtree(s->right, t);
+        return isEqual(s, t) || isSubtree(s->left, t) || isSubtree(s->right, t);
     }
     
-private:
     bool isEqual(TreeNode* p, TreeNode* t){
         if(!p || !t) return !p && !t;
-        if(p->val != t->val) return false;
-        return isEqual(p->left, t->left) && isEqual(p->right, t->right);
+        return p->val == t->val && isEqual(p->left, t->left) && isEqual(p->right, t->right);
     }
 };
