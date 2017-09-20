309bc98729074dd97676def15bcc4f046fe722ef
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-19 22:34:16 -0400

    Create 380. Insert Delete GetRandom O(1).cpp

---
+++ C++/380. Insert Delete GetRandom O(1).cpp
@@ -0,0 +1,41 @@
+class RandomizedSet {
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {}    
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if(m.count(val)) return false;
+        m[val] = v.size();
+        v.push_back(val);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if(!m.count(val)) return false;
+        int num = v.back();
+        m[num] = m[val];
+        v[m[val]] = num;
+        v.pop_back();
+        m.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        return v[rand() % v.size()];
+    }
+
+private:
+    unordered_map<int, int>m;
+    vector<int>v;
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * bool param_1 = obj.insert(val);
+ * bool param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
