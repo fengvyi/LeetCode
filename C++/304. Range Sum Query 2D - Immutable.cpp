e76c6beedb871dae08bd46a9af30f39d337cecf4
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-21 02:43:44 -0400

    Update 304. Range Sum Query 2D - Immutable.cpp

--- C++/304. Range Sum Query 2D - Immutable.cpp
+++ C++/304. Range Sum Query 2D - Immutable.cpp
@@ -1,3 +1,31 @@
+// See detailed explanation of that problem in discuss.
+class NumMatrix {
+public:
+    NumMatrix(vector<vector<int>> matrix) {
+        for(int i = 0; i < matrix.size(); i++){
+            dp.push_back(vector<int>());
+            for(int j = 0; j < matrix[0].size(); j++)
+                if(i == 0)
+                    if(j == 0) dp[i].push_back(matrix[0][0]);
+                    else dp[i].push_back(matrix[0][j] + dp[0][j - 1]);
+                else
+                    if(j == 0) dp[i].push_back(matrix[i][j] + dp[i - 1][0]);
+                    else dp[i].push_back(matrix[i][j] + dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1]);
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int a = (row1 == 0) ? 0 : dp[row1 - 1][col2];
+        int b = (col1 == 0) ? 0 : dp[row2][col1 - 1];
+        int c = (row1 == 0 || col1 == 0) ? 0 : dp[row1 - 1][col1 - 1];
+        return dp[row2][col2] - a - b + c;
+    }
+    
+private:
+    vector<vector<int>>dp;
+};
+
+// Or a more concise one, 7 lines.
 class NumMatrix {
 public:
     NumMatrix(vector<vector<int>> matrix) {
