d115cf9649be48a565683be5b67e12448c5c06fb
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-21 17:37:45 -0400

    Update 075. Sort Colors.cpp

--- C++/075. Sort Colors.cpp
+++ C++/075. Sort Colors.cpp
@@ -26,3 +26,16 @@ public:
             else j++;
     }
 };
+
+// And here is a O(k) space, O(k*n) time solution for k colors.
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int>color(3);
+        for(int i = 0; i < 3; i++){
+            if(i > 0) color[i] = color[i - 1];
+            for(int j = color[i]; j < nums.size(); j++)
+                while(color[i] <= j && nums[j] == i) swap(nums[j], nums[color[i]++]);
+        }
+    }
+};
