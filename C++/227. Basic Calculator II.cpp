4ef3f16ed092210971c35c44182656cb53a79cd8
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-22 00:37:13 -0400

    Update 227. Basic Calculator II.cpp

--- C++/227. Basic Calculator II.cpp
+++ C++/227. Basic Calculator II.cpp
@@ -26,16 +26,23 @@ public:
 class Solution {
 public:
     int calculate(string s) {
-        stringstream ss("+" + s + "+");
-        int sum(0), temp(0), num(0);
-        char op;
+        stringstream ss('+' + s + '+');
+        int temp = 0, num = 0, sum = 0;
+        char op = ' ';
         while(ss >> op){
             ss >> num;
-            if(op == '+' || op == '-'){
+            if(op == '+'){
                 sum += temp;
-                temp = (op == '+') ? num : -num;
+                temp = num;
             }
-            else temp = (op == '*') ? temp * num : temp / num;
+            if(op == '-'){
+                sum += temp;
+                temp = -num;
+            }
+            if(op == '*')
+                temp *= num;
+            if(op == '/')
+                temp /= num;
         }
         return sum;
     }
