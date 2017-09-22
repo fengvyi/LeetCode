9d558423e7577d6d2817ef6e2323e089563abd55
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-21 23:20:39 -0400

    Update 347. Top K Frequent Elements.cpp

--- C++/347. Top K Frequent Elements.cpp
+++ C++/347. Top K Frequent Elements.cpp
@@ -1,4 +1,5 @@
 // Solution 1. MaxHeap.
+// O(nlogn)
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -12,7 +13,27 @@ public:
     }
 };
 
+// O(nlog(n - k))
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int>m;
+        for(auto x: nums) m[x]++;
+        priority_queue<pair<int, int>>pq;
+        vector<int>res;
+        for(auto p: m){
+            pq.push({p.second, p.first});
+            if(pq.size() > m.size() - k){
+                res.push_back(pq.top().second);
+                pq.pop();
+            }
+        }
+        return res;
+    }
+};
+
 // Solution 2. MinHeap.
+// O(nlogk)
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
