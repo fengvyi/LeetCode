7a99b3a25964c12f04ddc59c457630ea7d90fbaa
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-22 14:29:05 -0400

    Update 075. Sort Colors.cpp

--- C++/075. Sort Colors.cpp
+++ C++/075. Sort Colors.cpp
@@ -32,7 +32,7 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int zero = 0, two = nums.size() - 1;
-        for(int i = 0; i < nums.size(); i++){
+        for(int i = 0; i <= two; i++){
             while(nums[i] == 2 && i < two) swap(nums[i], nums[two--]);
             while(nums[i] == 0 && zero < i) swap(nums[i], nums[zero++]);
         }
