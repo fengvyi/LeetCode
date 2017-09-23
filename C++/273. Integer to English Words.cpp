3df7fb1a2dca791e86637b405bb7a09321883d39
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-23 02:17:56 -0400

    Update 273. Integer to English Words.cpp

--- C++/273. Integer to English Words.cpp
+++ C++/273. Integer to English Words.cpp
@@ -1,33 +1,23 @@
-class Solution {
-private:
-    vector<string>LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",  "Eleven","Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    vector<string>TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    vector<string>THOUSANDS = {"", "Thousand", "Million", "Billion"};
+class Solution { 
 public:
     string numberToWords(int num) {
         if(num == 0) return "Zero";
+        vector<string>thousands({"", " Thousand", " Million", " Billion"});
+        string res = "";
         int i = 0;
-        string words = "";
-        
-        while(num > 0){
-            if(num % 1000 != 0)
-                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
+        while(num){
+            if(num % 1000) res = helper(num % 1000) + thousands[i] + (res.size() ? " " : "") + res;
             num /= 1000;
             i++;
         }
-        int j = words.size() - 1;
-        while(words[j] == ' ') j--;
-        return words.substr(0, j + 1);
+        return res;
     }
-
-    string helper(int num) {
-        if (num == 0)
-            return "";
-        else if (num < 20)
-            return LESS_THAN_20[num] + " ";
-        else if (num < 100)
-            return TENS[num / 10] + " " + helper(num % 10);
-        else
-            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+    
+    string helper(int num){
+        vector<string>lessThan20({"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine","Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"});
+        vector<string>tens({"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"});
+        if(num >= 100) return lessThan20[num / 100] + " Hundred" + (num % 100 ? " ": "") + helper(num % 100);
+        else if(num >= 20) return tens[num / 10] + (num % 10 ? " ": "") + helper(num % 10);
+        else return lessThan20[num];
     }
 };
