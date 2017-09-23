86f16a95b641e86c7266a2643efa7da6e471f327
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-23 14:22:15 -0400

    Update 283. Move Zeroes.cpp

--- C++/283. Move Zeroes.cpp
+++ C++/283. Move Zeroes.cpp
@@ -6,3 +6,16 @@ public:
         while(j < nums.size()) nums[j++] = 0;
     }
 };
+
+// Without maintaining the relative order, min steps.
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0, j = nums.size() - 1;
+        while(i < j){
+            while(nums[i] != 0) i++;
+            while(nums[j] == 0) j--;
+            swap(nums[i++], nums[j--]);
+        }
+    }
+};
