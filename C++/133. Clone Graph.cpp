c32ae43fc9e7082915d7fa4d633cd28550ed969e
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-24 23:18:35 -0700

    Update 133. Clone Graph.cpp

--- C++/133. Clone Graph.cpp
+++ C++/133. Clone Graph.cpp
@@ -6,18 +6,18 @@
  *     UndirectedGraphNode(int x) : label(x) {};
  * };
  */
-// Recursion
+// DFS
 class Solution {
 private:
-    unordered_map<int, UndirectedGraphNode*>m;
+    unordered_map<UndirectedGraphNode*, UndirectedGraphNode*>m;
 public:
     UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
         if(!node) return NULL;
-        if(m.count(node->label) == 0){
-            m[node->label] = new UndirectedGraphNode(node->label);
-            for(auto x: node->neighbors) m[node->label]->neighbors.push_back(cloneGraph(x));
+        if(m.count(node) == 0){
+            m[node] = new UndirectedGraphNode(node->label);
+            for(auto x: node->neighbors) m[node]->neighbors.push_back(cloneGraph(x));
         }
-        return m[node->label];
+        return m[node];
     }
 };
 
@@ -46,34 +46,5 @@ public:
             if(cur.empty()) swap(cur, next);
         }
         return root;
-    }
-};
-
-// DFS
-class Solution {
-private:
-    unordered_map<int, UndirectedGraphNode*>created;
-    unordered_map<int, int>visited;
-public:
-    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
-        if(!node) return NULL;
-        UndirectedGraphNode* root= new UndirectedGraphNode(node->label);
-        created[node->label] = root;
-        DFS(root, node);
-        return root;
-    }
-    
-    void DFS(UndirectedGraphNode* root, UndirectedGraphNode* node){
-        for(auto x: node->neighbors){
-            UndirectedGraphNode* copy;
-            if(created.count(x->label)) copy = created[x->label];
-            else{
-                copy = new UndirectedGraphNode(x->label);
-                created[x->label] = copy;
-            }
-            if(x != node && !visited.count(x->label)) DFS(copy, x);
-            root->neighbors.push_back(copy);
-            visited[x->label]++;
-        }
     }
 };
