313b8b0cc9ca14c705094f97c15a68f73932d74a
fengvyi <zefengsong@gwmail.gwu.edu>
2017-09-24 23:06:34 -0700

    Update 252. Meeting Rooms.cpp

--- C++/252. Meeting Rooms.cpp
+++ C++/252. Meeting Rooms.cpp
@@ -7,12 +7,25 @@
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
+// Solution 1.
 class Solution {
 public:
     bool canAttendMeetings(vector<Interval>& intervals) {
         if(intervals.empty()) return true;
         sort(intervals.begin(), intervals.end(),[](Interval& a,Interval& b){ return a.start < b.start; });
         for(int i = 0; i < intervals.size() - 1; i++) if(intervals[i].end > intervals[i + 1].start) return false;
+        return true;
+    }
+};
+
+// Solution 2.
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        map<int, int>m;
+        for(auto x: intervals) m[x.start]++, m[x.end]--;
+        int sum = 0;
+        for(auto x: m) if((sum += x.second) > 1) return false;
         return true;
     }
 };
