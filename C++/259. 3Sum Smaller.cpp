78a2b4cd75b2cafe0e05430e3127d5967f42468a
fengvyi <zefengsong@gwmail.gwu.edu>
2017-10-01 16:54:36 -0400

    Create 259. 3Sum Smaller.cpp

---
+++ C++/259. 3Sum Smaller.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int threeSumSmaller(vector<int>& nums, int target) {
+        int count = 0;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); i++)
+            for(int lo = i + 1, hi = nums.size() - 1; lo < hi; lo++){
+                while(lo < hi && nums[i] + nums[lo] + nums[hi] >= target) hi--;
+                count += hi - lo;
+            }
+        return count;
+    }
+};
