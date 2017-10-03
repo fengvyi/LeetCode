c473d2729bde502128fedfe1589f229aee3c2adb
fengvyi <zefengsong@gwmail.gwu.edu>
2017-10-02 20:26:45 -0400

    Create 362. Design Hit Counter.cpp

---
+++ C++/362. Design Hit Counter.cpp
@@ -0,0 +1,27 @@
+class HitCounter {
+private:
+    deque<int>q;
+public:
+    /** Initialize your data structure here. */
+    HitCounter() {}
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(int timestamp) {
+        q.push_back(timestamp);
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(int timestamp) {
+        while(!q.empty() && q.front() <= timestamp - 300) q.pop_front();
+        return q.size();
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
