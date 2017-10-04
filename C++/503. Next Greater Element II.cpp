d3b97cab8e01ad2f341586f13b57ea181ae80c3b
fengvyi <zefengsong@gwmail.gwu.edu>
2017-10-04 16:30:37 -0400

    Create 503. Next Greater Element II.cpp

---
+++ C++/503. Next Greater Element II.cpp
@@ -0,0 +1,41 @@
+// Solution 1. Brute force, O(n^2)
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>res;
+        for(int i = 0; i < nums.size(); i++){
+            int j = (i + 1) % n;
+            while(j != i){
+                if(nums[j] > nums[i]) break;
+                ++j %= n;
+            }
+            res.push_back(j == i ? -1 : nums[j]);
+        }
+        return res;
+    }
+};
+
+// Solution 2. Using a stack and a deque, O(n)
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int>res(nums.size(), -1);
+        stack<int>s; // decreasing sequence
+        deque<int>q; // Increasing sequence
+        for(int i = 0; i < nums.size(); i++){
+            if(q.empty() || nums[i] > q.back()) q.push_back(nums[i]);
+            while(!s.empty() && nums[i] > nums[s.top()]){
+                res[s.top()] = nums[i];
+                s.pop();
+            }
+            s.push(i);
+        }
+        while(!s.empty()){
+            while(!q.empty() && nums[s.top()] >= q.front()) q.pop_front();
+            if(!q.empty()) res[s.top()] = q.front();
+            s.pop();
+        }
+        return res;
+    }
+};
