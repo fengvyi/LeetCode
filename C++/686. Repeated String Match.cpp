5f24e78a5fcf396fec452176c4fff2858d1fc6b0
fengvyi <zefengsong@gwmail.gwu.edu>
2017-10-05 14:35:16 -0400

    Create 686. Repeated String Match.cpp

---
+++ C++/686. Repeated String Match.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int repeatedStringMatch(string A, string B) {
+        int i = 0, j = 0, count = 1;;
+        while(j < B.size()){
+            while(i < A.size() && A[i] != B[j]) i++;
+            if(i == A.size() || count > 1) return -1;
+            while(j < B.size() && A[i++] == B[j++]){
+                if(j == B.size()) return count;
+                if(i == A.size()) i = 0, count++;
+            }
+            j = 0;
+        }
+    }
+};
