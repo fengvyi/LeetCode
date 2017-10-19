3759fdbeff11ac3a0e79245c7a7a2a2427a064c4
fengvyi <zefengsong@gwmail.gwu.edu>
2017-10-19 14:55:38 -0400

    Create 654. Maximum Binary Tree.cpp

---
+++ C++/654. Maximum Binary Tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return DFS(nums, 0, nums.size());
+    }
+    
+    TreeNode* DFS(vector<int>& nums, int l, int r){
+        if(l == r) return NULL;
+        int maxPos = l;
+        for(int i = l; i < r; i++) if(nums[i] > nums[maxPos]) maxPos = i;
+        TreeNode* root = new TreeNode(nums[maxPos]);
+        root->left = DFS(nums, l, maxPos);
+        root->right = DFS(nums, maxPos + 1, r);
+        return root;
+    }
+};
