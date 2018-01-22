d4a78f88754b186db5cb5366c885bfb199b17279
fengvyi <zefengsong@gwmail.gwu.edu>
2018-01-21 23:17:21 -0500

    Update 746. Min Cost Climbing Stairs.cpp

--- C++/746. Min Cost Climbing Stairs.cpp
+++ C++/746. Min Cost Climbing Stairs.cpp
@@ -16,7 +16,6 @@ public:
 // If we take a look at the solution above, we can see the result dp[i] only depends on previous 2 steps(dp[i - 1] and dp[i - 2]).
 // So we can replace the 'dp' array with 2 variables.
 // Here is the final solution:
-
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
