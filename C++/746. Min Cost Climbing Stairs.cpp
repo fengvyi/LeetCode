281acd839965c6c4b94b37798dab4e1f0569b712
fengvyi <zefengsong@gwmail.gwu.edu>
2018-01-21 23:15:44 -0500

    Create 746. Min Cost Climbing Stairs.cpp

---
+++ C++/746. Min Cost Climbing Stairs.cpp
@@ -0,0 +1,29 @@
+// Recursion: `dp[i] = min{dp[i - 1], dp[i - 2]} + cost[i];`
+
+// A straightforward solution is:
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int>dp(n);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2; i < n; i++)
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+        return min(dp[n - 2], dp[n - 1]);
+    }
+};
+
+// If we take a look at the solution above, we can see the result `dp[i]` only depends on previous `2` steps(`dp[i - 1]` and `dp[i - 2]`). So we can replace the `dp` array with `2` variables.
+
+// Here is the final solution:
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size(), a = cost[0], b = cost[1], c;
+        for(int i = 2; i < n; i++, a = b, b = c)
+            c = min(a, b) + cost[i];
+        return min(a, b);
+    }
+};
