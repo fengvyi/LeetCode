253e1449c1f8e3179442b80bd9ded970e02e0e77
fengvyi <zefengsong@gwmail.gwu.edu>
2018-02-04 18:59:59 -0500

    Update 648. Replace Words.cpp

--- C++/648. Replace Words.cpp
+++ C++/648. Replace Words.cpp
@@ -1,3 +1,4 @@
+// Solution 1. Hash Table, 163 ms.
 class Solution {
 public:
     string replaceWords(vector<string>& dict, string sentence) {
@@ -15,5 +16,55 @@ public:
         }
         res.pop_back();
         return res;
+    }
+};
+
+// Solution 2.** Trie, 58 ms.
+struct TrieNode{
+    bool isKey;
+    TrieNode* next[26];
+    TrieNode(): isKey(false){
+        memset(next, NULL, sizeof(next));
+    }
+};
+
+class Solution {
+public:
+    string replaceWords(vector<string>& dict, string sentence) {
+        root = new TrieNode();
+        for(string& s: dict) buildTrie(s);
+        string res = "";
+        for(int i = 0; i < sentence.size();){
+            pair<string, int> nextWord = match(sentence, i);
+            res += nextWord.first + " ";
+            i = nextWord.second + 1;
+        }
+        res.pop_back();
+        return res;
+    }
+
+private:    
+    TrieNode* root;
+    
+    void buildTrie(string& s){
+        auto p = root;
+        for(char c: s){
+            if(!p->next[c - 'a']) p->next[c - 'a'] = new TrieNode();
+            p = p->next[c - 'a'];
+        }
+        p->isKey = true;
+    }
+    
+    pair<string, int> match(string& sentence, int pos){
+        auto p = root;
+        int i = pos;
+        while(i < sentence.size() && sentence[i] != ' ' && !p->isKey && p->next[sentence[i] - 'a']){
+            p = p->next[sentence[i] - 'a'];
+            i++;
+        }
+        int j = i;
+        while(i < sentence.size() && sentence[i] != ' ') i++;
+        if(p->isKey) return {sentence.substr(pos, j - pos), i};
+        return {sentence.substr(pos, i - pos), i};
     }
 };
