c187d50b173628e82900a40368073760a3cac803
fengvyi <zefengsong@gwmail.gwu.edu>
2018-04-02 10:39:28 -0400

    Create 364. Nested List Weight Sum II.cpp

---
+++ C++/364. Nested List Weight Sum II.cpp
@@ -0,0 +1,50 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    int depthSumInverse(vector<NestedInteger>& nestedList) {
+        int maxDepth = getDepth(nestedList);
+        return dfs(nestedList, maxDepth);
+    }
+    
+    int getDepth(vector<NestedInteger>& nestedList) {
+        int depth = 1;
+        for(auto x: nestedList)
+            if(!x.isInteger()) depth = max(depth, 1 + getDepth(x.getList()));
+        return depth;
+    }
+    
+    int dfs(vector<NestedInteger>& nestedList, int depth){
+        int res = 0;
+        for(auto x: nestedList)
+            res += x.isInteger() ? x.getInteger() * depth : dfs(x.getList(), depth - 1);
+        return res;
+    }
+};
