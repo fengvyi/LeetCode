254d4f1ca3ba77fa58edec33596743981bfb26b2
fengvyi <zefengsong@gwmail.gwu.edu>
2018-05-08 13:19:44 -0400

    Create 541. Reverse String II.cpp

---
+++ C++/541. Reverse String II.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.size();
+        for(int i = 0; i < n; i += 2*k)
+            reverse(s.begin() + i, s.begin() + min(i + k, n));
+        return s;
+    }
+};
