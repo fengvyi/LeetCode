a2a5a1483bbfd58bde25470b1048e79867a1600f
fengvyi <zefengsong@gwmail.gwu.edu>
2018-05-27 18:09:01 -0400

    Update 240. Search a 2D Matrix II.cpp

--- C++/240. Search a 2D Matrix II.cpp
+++ C++/240. Search a 2D Matrix II.cpp
@@ -29,11 +29,11 @@ public:
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.empty() || matrix[0].empty()) return false;
+        if(matrix.size() == 0) return false;
         int r = 0, c = matrix[0].size() - 1;
         while(r < matrix.size() && c >= 0){
             if(matrix[r][c] == target) return true;
-            matrix[r][c] < target ? r++ : c--;
+            matrix[r][c] > target ? c-- : r++;
         }
         return false;
     }
