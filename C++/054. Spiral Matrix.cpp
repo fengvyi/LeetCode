9f80b091c5b4f6c6d940e8e8d92972fdeebf6e9d
fengvyi <zefengsong@gwmail.gwu.edu>
2018-05-27 17:47:23 -0400

    Update 054. Spiral Matrix.cpp

--- C++/054. Spiral Matrix.cpp
+++ C++/054. Spiral Matrix.cpp
@@ -1,3 +1,24 @@
+// Final solution
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>res;
+        if(matrix.size() == 0) return res;
+        int minR = 0, maxR = matrix.size() - 1, minC = 0, maxC = matrix[0].size() - 1;
+        while(minR <= maxR && minC <= maxC){
+            for(int i = minC; i <= maxC; i++) res.push_back(matrix[minR][i]);
+            minR++;
+            for(int i = minR; i <= maxR; i++) res.push_back(matrix[i][maxC]);
+            maxC--;
+            for(int i = maxC; i >= minC && minR <= maxR; i--) res.push_back(matrix[maxR][i]);
+            maxR--;
+            for(int i = maxR; i >= minR && minC <= maxC; i--) res.push_back(matrix[i][minC]);
+            minC++;
+        }
+        return res;
+    }
+};
+
 // Solution 1.
 class Solution {
 public:
