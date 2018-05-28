99f571841ae45d67d6ce03d3426e7081c4e7e9ef
fengvyi <zefengsong@gwmail.gwu.edu>
2018-05-27 22:03:44 -0400

    Update 074. Search a 2D Matrix.cpp

--- C++/074. Search a 2D Matrix.cpp
+++ C++/074. Search a 2D Matrix.cpp
@@ -1,3 +1,4 @@
+// Solution 1
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
@@ -13,3 +14,21 @@ public:
         return p != matrix[lo].end() && *p == target;
     }
 };
+
+// Solution 2
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.size() == 0) return false;
+        int m = matrix.size(), n = matrix[0].size(), l = 0, r = m * n - 1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            int val = matrix[mid/n][mid%n];
+            if(val > target) r = mid - 1;
+            else if(val < target) l = mid + 1;
+            else return true;
+        }
+        return false;
+    }
+};
+
