a1ebe0139f208e1e9c68d83748b67d859d34e9f9
fengvyi <zefengsong@gwmail.gwu.edu>
2018-05-27 23:12:12 -0400

    Update 168. Excel Sheet Column Title.cpp

--- C++/168. Excel Sheet Column Title.cpp
+++ C++/168. Excel Sheet Column Title.cpp
@@ -11,3 +11,17 @@ public:
         return res;
     }
 };
+
+// Or
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string res = "";
+        while(n){
+            res.push_back('A' + (n - 1)%26);
+            n = (n - 1) / 26;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
