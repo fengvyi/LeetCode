48412ee8700cc158cb63fcd3029e243e6c935ac9
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-09 21:22:24 -0500

    Update 914. X of a Kind in a Deck of Cards.cpp

--- C++/914. X of a Kind in a Deck of Cards.cpp
+++ C++/914. X of a Kind in a Deck of Cards.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    bool hasGroupsSizeX(vector<int>& deck) 
-    {
+    bool hasGroupsSizeX(vector<int>& deck) {
         unordered_map<int, int>m;
         int n = deck.size();
-        for (int i = 0; i < n; ++i)
-        {
+        for (int i = 0; i < n; ++i) {
             m[deck[i]]++;
         }
         int base = 0;
-        for (auto& p: m)
-        {
+        for (auto& p: m) {
             base = gcd(p.second, base);
         }
         return base > 1;
     }
     
-    int gcd(int a, int b)
-    {
-        if (b == 0)
-        {
+    int gcd(int a, int b) {
+        if (b == 0) {
             return a;
         }
         return gcd(b, a % b);
