85785e97183577f2ae65cd05b5b02e4ddae69c42
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-17 14:02:51 -0500

    Update 450. Delete Node in a BST.cpp

--- C++/450. Delete Node in a BST.cpp
+++ C++/450. Delete Node in a BST.cpp
@@ -28,3 +28,63 @@ public:
         cur == pre->left ? pre->left = cur->left : pre->right = cur->left;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) {
+            return NULL;
+        }
+        TreeNode* head = new TreeNode(0);
+        head->left = root;
+        TreeNode* parent;
+        TreeNode* node = dfs(root, head, key, parent);
+        if (!node) {
+            return root;
+        }
+        
+        TreeNode* l = node->left;
+        TreeNode* r = node->right;
+        TreeNode* p = r;
+        while (p && p->left) {
+            p = p->left;
+        }
+        if (!r) {
+            r = l;
+        } else if (p) {
+            p->left = l;
+        }
+        if (node == parent->left) {
+            parent->left = r;
+        } else {
+            parent->right = r;
+        }
+        return head->left;
+    }
+    
+    TreeNode* dfs(TreeNode* cur, TreeNode* pre, int key, TreeNode*& parent) {
+        if (!cur) {
+            return NULL;
+        }
+        if (cur->val == key) {
+            parent = pre;
+            return cur;
+        }
+        
+        if (cur->val > key) {
+            return dfs(cur->left, cur, key, parent);
+        } else {
+            return dfs(cur->right, cur, key, parent);
+        }
+    }
+};
