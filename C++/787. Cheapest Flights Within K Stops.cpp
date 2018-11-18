638f5dcbceaf3dc5f9189ae247f5aaee9d722b77
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-18 12:32:15 -0500

    Update 787. Cheapest Flights Within K Stops.cpp

--- C++/787. Cheapest Flights Within K Stops.cpp
+++ C++/787. Cheapest Flights Within K Stops.cpp
@@ -1,3 +1,4 @@
+// DFS + Brute Force
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
@@ -15,5 +16,45 @@ public:
         }
         if(K == -1 || price >= minPrice) return;
         for(auto v: g[cur]) dfs(g, v[0], dst, K - 1, price + v[1], minPrice);
+    }
+};
+
+// BFS + Priority_queue
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        vector<vector<int>>g(101);
+        vector<vector<int>>w(101, vector<int>(101));
+        for (auto& v: flights) {
+            int a = v[0];   // src
+            int b = v[1];   // dst
+            int c = v[2];   // weight
+            g[a].push_back(b);
+            w[a][b] = c;
+        }
+        auto comp = [](vector<int>& v1, vector<int>& v2) {
+          return v1[1] > v2[1];  
+        };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)>pq(comp);
+        pq.push({src, 0, K});
+        while (!pq.empty()) {
+            auto v = pq.top();
+            pq.pop();
+            int from = v[0];
+            int cost = v[1];
+            int stop = v[2];
+            
+            if (from == dst) {
+                return cost;
+            }
+            if (stop < 0) {
+                continue;
+            }
+            --stop;
+            for (int x: g[from]) {
+                pq.push({x, cost + w[from][x], stop});
+            }
+        }
+        return -1;
     }
 };
