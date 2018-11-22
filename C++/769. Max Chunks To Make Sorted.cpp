4d8fab118bfc025a73b53074e329c450186e9451
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-22 11:39:31 -0500

    Update 769. Max Chunks To Make Sorted.cpp

--- C++/769. Max Chunks To Make Sorted.cpp
+++ C++/769. Max Chunks To Make Sorted.cpp
@@ -9,3 +9,17 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int curMax = 0, n = arr.size(), res = 0;
+        for (int i = 0; i < n; ++i) {
+            curMax = max(curMax, arr[i]);
+            if (curMax == i) {
+                ++res;
+            }
+        }
+        return res;
+    }
+};
