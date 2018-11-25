38dbaa36ab91c664cfeb9eb69ff4fdb54f60e38c
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-25 15:29:48 -0500

    Update 399. Evaluate Division.cpp

--- C++/399. Evaluate Division.cpp
+++ C++/399. Evaluate Division.cpp
@@ -37,3 +37,65 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<double> calcEquation(vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries) {
+        vector<double>res;
+        unordered_map<string, vector<string>>g;
+        unordered_map<string, unordered_map<string, double>>w;
+        // Build graph
+        for (int i = 0; i < equations.size(); ++i) {
+            string a = equations[i].first;
+            string b = equations[i].second;
+            g[a].push_back(b);
+            g[b].push_back(a);
+            w[a][b] = values[i];
+            w[b][a] = 1/values[i];
+        }
+        
+        for (auto& q: queries) {
+            string from = q.first;
+            string dest = q.second;
+            
+            if (!g.count(from) || !g.count(dest)) {
+                res.push_back(-1);
+                continue;
+            }
+            
+            // BFS
+            unordered_set<string>visited;
+            queue<pair<string, double>>cur, next;
+            cur.push({from, 1});
+            bool found = false;
+            while(!cur.empty()) {
+                auto node = cur.front();
+                cur.pop();
+                
+                string s = node.first;
+                double val = node.second;
+                
+                visited.insert(s);
+                
+                if (s == dest) {
+                    res.push_back(val);
+                    found = true;
+                    break;
+                }
+                
+                for (auto& x: g[s]) {
+                    if (!visited.count(x)) {
+                        next.push({x, val * w[s][x]});
+                    }
+                }
+                if (cur.empty()) {
+                    swap(cur, next);
+                }
+            }
+            if (!found) {
+                res.push_back(-1);
+            }
+        }
+        return res;
+    }
+};
