5ec9662e7cace9e2b3ccc0f299f6751475e87644
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-25 22:12:29 -0500

    Update 340. Longest Substring with At Most K Distinct Characters.cpp

--- C++/340. Longest Substring with At Most K Distinct Characters.cpp
+++ C++/340. Longest Substring with At Most K Distinct Characters.cpp
@@ -11,3 +11,24 @@ public:
         return maxlen;
     }
 };
+
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int count = 0, j = 0, res = 0;
+        unordered_map<char, int>m;
+        for (int i = 0; i < s.size(); ++i) {
+            if (m[s[i]]++ == 0) {
+                ++count;
+            }
+            
+            while (count > k) {
+                if (--m[s[j++]] == 0) {
+                    --count;
+                }
+            }
+            res = max(res, i - j + 1);
+        }
+        return res;
+    }
+};
