cfe0051d934b7b1f8c3df7de76ccc89a7ab2ed68
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-26 21:11:15 -0500

    Update 200. Number of Islands.cpp

--- C++/200. Number of Islands.cpp
+++ C++/200. Number of Islands.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.size() == 0 || grid[0].size() == 0) return 0;
-        int count = 0;
-        int m = grid.size(), n = grid[0].size();
-        for(int i = 0; i < m; i++)
-            for(int j = 0; j < n; j++)
-                if(grid[i][j] == '1') count++, BFS(grid, i, j, m, n);
-        return count;
+        if (grid.empty()) {
+            return 0;
+        }
+        int res = 0, m = grid.size(), n = grid[0].size();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '1') {
+                    ++res;
+                    dfs(grid, i, j, m, n);
+                }
+            }
+        }
+        return res;
     }
     
-    void BFS(vector<vector<char>>& grid, int r, int c, int m, int n){
-        if(r < 0 || c < 0 || r >= m || c >= n || grid[r][c] == '0') return;
+    void dfs(vector<vector<char>>& grid, int r, int c, int& m, int& n) {
+        if (r < 0 || c < 0 || r == m || c == n || grid[r][c] == '0') {
+            return;
+        }
         grid[r][c] = '0';
-        BFS(grid, r - 1, c, m, n);
-        BFS(grid, r + 1, c, m, n);
-        BFS(grid, r, c + 1, m, n);
-        BFS(grid, r, c - 1, m, n);
+        dfs(grid, r + 1, c, m, n);
+        dfs(grid, r - 1, c, m, n);
+        dfs(grid, r, c - 1, m, n);
+        dfs(grid, r, c + 1, m, n);
     }
 };
