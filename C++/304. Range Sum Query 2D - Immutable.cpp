6317a1922c0620fa5e2f0db64d6365566c313565
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-28 16:06:56 -0500

    Update 304. Range Sum Query 2D - Immutable.cpp

--- C++/304. Range Sum Query 2D - Immutable.cpp
+++ C++/304. Range Sum Query 2D - Immutable.cpp
@@ -29,18 +29,30 @@ private:
 class NumMatrix {
 public:
     NumMatrix(vector<vector<int>> matrix) {
-        for(int i = 0; i < matrix.size(); i++){
-            dp.push_back(vector<int>());
-            for(int j = 0; j < matrix[0].size(); j++)
-                if(i == 0) dp[i].push_back(!j ? matrix[0][0] : matrix[0][j] + dp[0][j - 1]);
-                else dp[i].push_back(!j ? matrix[i][j] + dp[i - 1][0] : matrix[i][j] + dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1]);
+        if (matrix.empty() || matrix[0].empty()) {
+            return;
+        }
+        int m = matrix.size(), n = matrix[0].size();
+        dp = vector<vector<int>>(m + 1, vector<int>(n + 1));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                dp[i + 1][j + 1] = dp[i][j + 1] + dp[i + 1][j] - dp[i][j] + matrix[i][j];
+            }
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return dp[row2][col2] - (!row1 ? 0 : dp[row1 - 1][col2]) - (!col1 ? 0 : dp[row2][col1 - 1]) + (!row1 || !col1 ? 0 : dp[row1 - 1][col1 - 1]);
+        return dp[row2 + 1][col2 + 1] - dp[row2 + 1][col1] - dp[row1][col2 + 1] + dp[row1][col1];
     }
     
 private:
     vector<vector<int>>dp;
 };
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
+
+
