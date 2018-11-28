974944732066e70be4b5bc8b2eec5fe380cc12e2
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-28 16:56:22 -0500

    Update 307. Range Sum Query - Mutable.cpp

--- C++/307. Range Sum Query - Mutable.cpp
+++ C++/307. Range Sum Query - Mutable.cpp
@@ -91,3 +91,83 @@ public:
  * obj.update(i,val);
  * int param_2 = obj.sumRange(i,j);
  */
+
+
+class NumArray {
+    struct TreeNode {
+        int start;
+        int end;
+        int sum;
+        TreeNode* left;
+        TreeNode* right;
+        TreeNode(int x, int y):start(x), end(y), sum(0), left(NULL), right(NULL) {}
+    };
+public:
+    NumArray(vector<int> nums) {
+        root = buildTree(nums, 0, nums.size() - 1);
+    }
+    
+    void update(int i, int val) {
+        dfs(root, i, val);
+    }
+    
+    int sumRange(int i, int j) {
+        return i == 0 ? getSum(root, j) : getSum(root, j) - getSum(root, i - 1);
+    }
+    
+    int getSum(TreeNode* root, int idx) {
+        if (root->end == idx) {
+            return root->sum;
+        }
+        int mid = root->start + (root->end - root->start)/2;
+        if (idx <= mid) {
+            return getSum(root->left, idx);
+        } else {
+            return root->left->sum + getSum(root->right, idx);
+        }
+    }
+    
+    
+    void dfs(TreeNode* root, int i, int val) {
+        if (root->start == i && root->end == i) {
+            root->sum = val;
+            return;
+        }
+        
+        int mid = root->start + (root->end - root->start)/2;
+        
+        if (i <= mid) {
+            dfs(root->left, i, val);
+        } else {
+            dfs(root->right, i, val);
+        }
+        root->sum = root->left->sum + root->right->sum;
+    }
+    
+    TreeNode* buildTree(vector<int>& nums, int start, int end) {
+        if (start > end) {
+            return NULL;
+        }
+        if (start == end) {
+            TreeNode* node = new TreeNode(start, end);
+            node->sum = nums[start];
+            return node;
+        }
+        TreeNode* node = new TreeNode(start, end);
+        int mid = start + (end - start)/2;
+        node->left = buildTree(nums, start, mid);
+        node->right = buildTree(nums, mid + 1, end);
+        node->sum = node->left->sum + node->right->sum;
+        return node;
+    }
+    
+private:
+    TreeNode* root;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
