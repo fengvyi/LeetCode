2a02ff466a914726d9c2e01b2422bc61e2b8fb84
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-27 23:52:54 -0500

    Update 015. 3Sum.cpp

--- C++/015. 3Sum.cpp
+++ C++/015. 3Sum.cpp
@@ -15,3 +15,36 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>res;
+        for (int l = 0; l < n - 2; ++l) {
+            if (l > 0 && nums[l] == nums[l - 1]) {
+                continue;
+            }
+            int r = n - 1;
+            int mid = l + 1;
+            while (mid < r) {
+                if (r < n - 1 && nums[r] == nums[r + 1]) {
+                    --r;
+                    continue;
+                }
+                
+                while (r > mid && nums[l] + nums[mid] + nums[r] < 0) {
+                    ++mid;
+                }
+                
+                if (r > mid && nums[l] + nums[mid] + nums[r] == 0) {
+                    res.push_back({nums[l], nums[mid], nums[r]});
+                }
+                --r;
+            }
+        }
+        return res;
+    }
+};
