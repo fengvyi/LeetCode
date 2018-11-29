4f669d5eca41906e1ae8daaddc78f0db67128325
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-29 09:53:45 -0500

    Update 268. Missing Number.cpp

--- C++/268. Missing Number.cpp
+++ C++/268. Missing Number.cpp
@@ -17,3 +17,20 @@ public:
         return (1 + n) * n/2 - sum;
     }
 };
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int l = 0, r = nums.size() - 1, mid;
+        while (l <= r) {
+            mid = l + (r - l)/2;
+            if (nums[mid] > mid) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
