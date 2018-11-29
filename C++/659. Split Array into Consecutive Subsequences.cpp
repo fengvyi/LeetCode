2f76a3721f53e1101da63761ce555fbb647ef30b
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-29 15:50:33 -0500

    Update 659. Split Array into Consecutive Subsequences.cpp

--- C++/659. Split Array into Consecutive Subsequences.cpp
+++ C++/659. Split Array into Consecutive Subsequences.cpp
@@ -20,3 +20,31 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int, int>m, t;
+        for (int& x: nums) {
+            ++m[x];
+        }
+        
+        for (int& x: nums) {
+            if (!m[x]) {
+                continue;
+            }
+            --m[x];
+            if (t[x]) {
+                --t[x];
+                ++t[x + 1];
+            } else if (m[x + 1] && m[x + 2]) {
+                --m[x + 1];
+                --m[x + 2];
+                ++t[x + 3];
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
