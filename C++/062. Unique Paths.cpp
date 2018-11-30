f9f1ce2f5fb364d463f7db2e1560dc5351b504ed
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-29 21:27:15 -0500

    Update 062. Unique Paths.cpp

--- C++/062. Unique Paths.cpp
+++ C++/062. Unique Paths.cpp
@@ -10,3 +10,31 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m + 1, vector<int>(n + 1));
+        dp[1][1] = 1;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                dp[i][j] += dp[i -  1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int>dp(n);
+        dp[0] = 1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                dp[j] += dp[j -  1];
+            }
+        }
+        return dp[n - 1];
+    }
+};
