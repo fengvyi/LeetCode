ca645f0c1a246a876b77ab4e5eb791bb9b2a8ee4
fengvyi <zefengsong@gwmail.gwu.edu>
2018-11-29 21:34:41 -0500

    Update 063. Unique Paths II.cpp

--- C++/063. Unique Paths II.cpp
+++ C++/063. Unique Paths II.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if(obstacleGrid.empty()) return 0;
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<vector<int>>dp(m, vector<int>(n, 0));
-        dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
-        for(int i = 1; i < m; i++) dp[i][0] = !dp[i - 1][0] || obstacleGrid[i][0] ? 0 : 1;
-        for(int i = 1; i < n; i++) dp[0][i] = !dp[0][i - 1] || obstacleGrid[0][i] ? 0 : 1;
-        for(int i = 1; i < m; i++)
-            for(int j = 1; j < n; j++)
-                if(!obstacleGrid[i][j]) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-        return dp[m - 1][n - 1];
+        vector<int>dp(n);
+        dp[0] = 1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } else {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[n - 1];
     }
 };
