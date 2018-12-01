ccf4f8feaf5b298dd9586d22b5b4c8c45f3f9202
fengvyi <zefengsong@gwmail.gwu.edu>
2018-12-01 10:31:44 -0500

    Update 100. Same Tree.cpp

--- C++/100. Same Tree.cpp
+++ C++/100. Same Tree.cpp
@@ -1,33 +1,65 @@
 // DFS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q) return true;
-        if(!p || !q || p->val != q->val) return false;
+        if (!p || !q) {
+            return !p && !q;
+        }
+        if (p->val != q->val) {
+            return false;
+        }
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
 
 // BFS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        deque<TreeNode*>q1, q2;
-        if(p) q1.push_back(p);
-        if(q) q2.push_back(q);
-        while(!q1.empty() && !q2.empty()){
-            TreeNode* a = q1.front();
-            TreeNode* b = q2.front();
-            q1.pop_front();
-            q2.pop_front();
-            if(a->val != b->val) return false;
-            if((a->left || b->left) && (!a->left || !b->left)) return false;
-            if((a->right || b->right) && (!a->right || !b->right)) return false;
-            if(a->left) q1.push_back(a->left);
-            if(a->right) q1.push_back(a->right);
-            if(b->left) q2.push_back(b->left);
-            if(b->right) q2.push_back(b->right);
+        queue<TreeNode*>pq, qq;
+        pq.push(p);
+        qq.push(q);
+        while(!pq.empty() && !qq.empty()) {
+            auto a = pq.front();
+            pq.pop();
+            auto b = qq.front();
+            qq.pop();
+            if (!a || !b) {
+                if (!a && !b) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            
+            if (a->val != b->val) {
+                return false;
+            }
+            
+            pq.push(a->left);
+            pq.push(a->right);
+            
+            qq.push(b->left);
+            qq.push(b->right);
         }
-        return q1.empty() && q2.empty();
+        return true;
     }
 };
