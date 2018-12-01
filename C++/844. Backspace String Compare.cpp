3f7a94c10e1dc882d8ed05c4e271a1d08fd2815f
fengvyi <zefengsong@gwmail.gwu.edu>
2018-12-01 17:00:15 -0500

    Update 844. Backspace String Compare.cpp

--- C++/844. Backspace String Compare.cpp
+++ C++/844. Backspace String Compare.cpp
@@ -4,8 +4,8 @@ public:
     bool backspaceCompare(string S, string T) {
         int i = S.size() - 1, j = T.size() - 1, countA = 0, countB = 0;
         while(i >= 0 || j >= 0){
-            while(i >= 0 && (S[i] == '#' || countA > 0)) S[i--] == '#' ? countA++ : countA--;
-            while(j >= 0 && (T[j] == '#' || countB > 0)) T[j--] == '#' ? countB++ : countB--;
+            while(i >= 0 && (S[i] == '#' || countA > 0)) S[i--] == '#' ? ++countA : --countA;
+            while(j >= 0 && (T[j] == '#' || countB > 0)) T[j--] == '#' ? ++countB : --countB;
             if(i < 0 || j < 0) return i == j;
             if(S[i--] != T[j--]) return false;
         }
