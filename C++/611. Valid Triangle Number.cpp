3c3fbd6d93e1b115eaf17db41e416cf9b1b9a6d8
fengvyi <zefengsong@gwmail.gwu.edu>
2018-12-02 16:44:19 -0800

    Update 611. Valid Triangle Number.cpp

--- C++/611. Valid Triangle Number.cpp
+++ C++/611. Valid Triangle Number.cpp
@@ -10,3 +10,23 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int count = 0, n = nums.size();
+        for(int i = n - 1; i > 1; --i) {
+            int l = 0, r = i - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] > nums[i]) {
+                    count += r - l;
+                    --r;
+                } else {
+                    ++l;
+                }
+            }
+        }
+        return count;
+    }
+};
